87d0f967729a60b846e3642148f39d6f812e064f
Daniel Bailey <danieljabailey@gmail.com>
2017-02-23 19:26:28 +0000

    Added file writer

---
+++ FileWriter.py
@@ -0,0 +1,14 @@
+class FileWriter(object):
+
+	def __init__(self, filename):
+		self.filename = filename
+
+	"""write outputData to file, outputData is a dict where the
+		cache ID is the key and the values are lists of video IDs
+	"""
+	def writeData(self, outputData)
+		file = open(self.filename, "w+")
+		file.write("%d\n" % (len(outputData)))
+		for cID, vidList in outputData.items():
+			file.write("%d %s" % (cID, " ".join(vidList)))
+		file.close()
