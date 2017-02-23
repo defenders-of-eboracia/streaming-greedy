dff68e82170fee93de51f9687cfd7c41838260cb
Daniel Bailey <danieljabailey@gmail.com>
2017-02-23 18:54:38 +0000

    Added some transformation features to file parser

--- FileReader.py
+++ FileReader.py
@@ -4,10 +4,17 @@ class Endpoint(object):
 	def __init__(self, latency, nCaches):
 		self.latency = latency
 		self.nCaches = nCaches
-		self.cacheServers = []
+		self.cacheServers = {}
+		self.requests = {}
 
 	def addCacheServer(self, cID, latency):
-		self.cacheServers.append((cID, latency))
+		self.cacheServers[cID] = latency
+
+	def addRequests(self, vID, requests):
+		if vID not in self.requests:
+			self.requests[vID] = requests
+			return
+		self.requests[vID] += requests
 
 	def check(self):
 		assert(self.nCaches == len(self.cacheServers))
@@ -16,7 +23,7 @@ class Endpoint(object):
 		return self.__repr__()
 
 	def __repr__(self):
-		return ",".join("%d: latency=%d" % (cID, latency) for (cID, latency) in self.cacheServers)
+		return ",".join("%d: latency=%d" % (cID, latency) for (cID, latency) in self.cacheServers.items())
 
 class FileReader(object):
 
@@ -37,13 +44,30 @@ class FileReader(object):
 			self.endpoints.append(thisEndpoint)
 		self.requests = []
 		for r in range(self.nReqDescs):
-			vID, eID, latency = [int(x) for x in self.file.readline().split(" ")]
-			self.requests.append((vID, eID, latency))
+			vID, eID, numReqs = [int(x) for x in self.file.readline().split(" ")]
+			self.requests.append((vID, eID, numReqs))
+
+	def transFormVideoRequests(self):
+		self.videoRequests = {}
+		for vID in range(self.nVideos):
+			self.videoRequests[vID] = []
+		for (vID, eID, numReqs) in self.requests:
+			self.videoRequests[vID].append((eID, numReqs))
+
+	def augmentEndpoints(self):
+		self.endpointById = {}
+		for eID, endPoint in enumerate(self.endpoints):
+			endPoint.id = eID
+			self.endpointById[eID] = endPoint
+		for (vID, eID, numReqs) in self.requests:
+			self.endpointById[eID].addRequests(vID, numReqs)
 
 	def __init__(self, filename):
 		self.filename = filename
 		self.file = open(self.filename, "r")
 		self.parseFile()
+		self.transFormVideoRequests()
+		self.augmentEndpoints()
 
 if __name__ == "__main__":
 	testfile = "trivialExample.in"
@@ -51,3 +75,5 @@ if __name__ == "__main__":
 	print("Video Sizes: %r" % (inputData.videoSizes))
 	print("Endpoints:\n\t%s" % ("\n\t".join([str(e) for e in inputData.endpoints])))
 	print("Requests: %r" % ([r for r in inputData.requests]))
+	print(inputData.videoRequests)
+	print(inputData.endpointById)
